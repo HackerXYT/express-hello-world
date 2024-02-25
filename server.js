5f2f05b15cd165d2329b652bc93317065381a7a7
Gregory Papapostolou <88238306+HackerXYT@users.noreply.github.com>
2024-02-25 16:33:48 +0200

    a

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-const app = require('./app')
+const express = require("express")
 
-const port = process.env.PORT || 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const app = express()
+//const apo = express()
+
+app.use(express.static(__dirname + "/public/"), (_, res, next) => {
+	//res.status(404)
+  //res.sendFile(__dirname + "/404/")
+  });
+  
+  app.listen(8080);
